0ea2e142b6667647144f99f763383bbf4b112f1c
zengsh <gregzeng@qq.com>
2019-03-10 14:07:34 +0000

    run.sh script modified

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # MA+DD pairs
-for SNR in 50 0 -5 -20 ; do
+for SNR in -50 -10 5 10 ; do
     echo "***** MA+DD SNR${SNR} *****" 
     rm -rf ./models/simple_tag/tag_MA_DD_SNR${SNR}/ 
     python3 main.py simple_tag tag_MA_DD_SNR${SNR} \
@@ -12,10 +12,10 @@ for SNR in 50 0 -5 -20 ; do
 done
 
 # only 1 DD+DD pair
-echo "***** DD+DD *****"
-rm -rf ./models/simple_tag/tag_DD_DD
-python3 main.py simple_tag tag_DD_DD \
-    --agent_alg DDPG \
-    --adversary_alg DDPG \
-    --validate_every_n_eps 100 \
-    --run_n_eps_in_validation 10
+#echo "***** DD+DD *****"
+#rm -rf ./models/simple_tag/tag_DD_DD
+#python3 main.py simple_tag tag_DD_DD \
+#    --agent_alg DDPG \
+#    --adversary_alg DDPG \
+#    --validate_every_n_eps 100 \
+#    --run_n_eps_in_validation 10
